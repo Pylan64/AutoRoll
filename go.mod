2c5af21e354d3423b9032a8f014005b2a9ba217f
Pylan64 <87488451+Pylan64@users.noreply.github.com>
2022-11-08 09:30:28 -0600

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.19
+
+require github.com/toqueteos/webbrowser v1.2.0 // indirect
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/toqueteos/webbrowser"
+
+func main() {
+	webbrowser.Open("https://www.python.org/ftp/python/3.11.0/python-3.11.0-macos11.pkg")
+}
